2ed071319bb138a9c6dccfb4b84b3b2e2ac8eb55
yunl.zheng <yunl.zheng@gmail.com>
2018-01-10 16:59:24 +0800

    add ch1 samples

---
+++ ch1/server.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+)
+
+var mu sync.Mutex
+var count int
+
+var palette = []color.Color{color.White, color.Black}
+
+const (
+	whiteIndex = 0
+	blackIndex = 1
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	mu.Unlock()
+	// fmt.Fprintf(w, "URL.Path =  %q \n", r.URL.Path)
+	lissajous(w)
+}
+
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	mu.Unlock()
+	fmt.Fprintf(w, "Count =  %d \n", count)
+}
+
+func lissajous(out io.Writer) {
+	const (
+		cycles = 5
+		res = 0.001
+		size = 100
+		nframes = 64
+		delay = 8
+	)
+
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: nframes}
+
+	phase := 0.0
+	for i := 0; i< nframes; i++ {
+		rect := image.Rect(0,0,2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+
+		for t := 0.0; t < cycles * 2 * math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq+phase)
+			img.SetColorIndex(size + int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image  = append(anim.Image, img)
+	}
+
+	
+
+	gif.EncodeAll(out, &anim)
+
+}
